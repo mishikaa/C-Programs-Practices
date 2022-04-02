fec7fdb5380d662f1e50ada99f90189e744c7ad5
Mishika Jaiswal <mishikaj162001@gmail.com>
2022-04-02 17:44:46 +0530

    Updated ArrayElementsReverse.c

--- Arrays/ArrayElementsReverse.c
+++ Arrays/ArrayElementsReverse.c
@@ -3,25 +3,30 @@
 int main()
 {
     int n,temp;
+    
+    prinf("Enter the length of array :\n);
     scanf("%d",&n);
+          
     int arr[n];
-    for(int i=0;i<n;i++)
+          
+    for( int i = 0 ; i < n ; i++ )
     {
         scanf("%d",&arr[i]);
     }
+          
     printf("ORIGINAL ARRAY:\n");
-    for(int i=0;i<n;i++)
+    for(int i = 0 ; i < n ; i++)
     {
         printf("%d ",arr[i]);
     }
-    for(int i=0;i<n/2;i++)
+    for(int i = 0 ; i < n/2 ; i++)
     {
-       temp=arr[i];
-       arr[i]=arr[n-i-1];
-       arr[n-i-1]=temp;
+       temp = arr[i];    //Reversing array elements
+       arr[i] = arr[n-i-1]; 
+       arr[n-i-1] = temp;
     }
     printf("\nARRAY IN REVERSE ORDER:\n");
-    for(int i=0;i<n;i++)
+    for(int i = 0 ; i < n ; i++)
     {
         printf("%d ",arr[i]);
     }
