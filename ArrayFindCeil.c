93ac423e83f07edaeb403ff1ed30b2b4902c9631
Mishika Jaiswal <bcs_2021043@iiitm.ac.in>
2022-04-02 17:25:06 +0530

    Find the ceiling in a sorted array
    Ceiling of x is the smallest element in array greater than or equal to x
    1)Input :
    Enter the size of array : 5
    
    Enter elements in array : 1 4 6 7 9
    Find the ceiling of : 4
    Output :
    The given array is : 1 4 6 7 9 
    The ceiling of 4 is : 4        
    
    2)Input :
    Enter the size of array : 4
    
    Enter elements in array : 4 7 9 10 
    Find the ceiling of : 5
    Output :
    The given array is : 4 7 9 10
    The ceiling of 5 is : 7

---
+++ ArrayFindCeil.c
@@ -0,0 +1,51 @@
+//PROGRAM TO FIND THE CEILING IN A SORTED ARRAY
+
+#include<stdio.h>
+
+    void printarray(int *arr,int n);
+    int Findceil(int *arr,int n,int ceil);
+
+int main()
+{  
+    int n;
+    printf("Input :\n");
+    printf("Enter the size of array : ");
+    scanf("%d",&n);
+    printf("\nEnter elements in array : ");
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+    scanf("%d",&arr[i]);
+    }
+     printf("Find the ceiling of : ");
+     int num;
+     scanf("%d",&num);
+
+     int ans = Findceil(arr,n,num); 
+     printf("Output :\n");
+     printf("The given array is : ");
+     printarray(arr,n);
+     printf("\nThe ceiling of %d is : %d",num,ans);
+
+    return 0;
+}
+
+    int Findceil(int *arr,int n,int num)
+    {
+        for(int i=0;i<n;i++)
+        {
+            if(*(arr+i)>=num) //Check if array element is greater than or equal to num 
+            {
+                return *(arr+i);
+            }
+            
+        }
+    }  
+
+    void printarray(int *arr,int n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            printf("%d ",arr[i]);
+        }
+    }
