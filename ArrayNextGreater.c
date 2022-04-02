7215ff535c59bb74416f46534b8358e89d09c596
Mishika Jaiswal <bcs_2021043@iiitm.ac.in>
2022-04-02 17:28:57 +0530

    To find the next greater element for each element
    Test case 1 : 
    The given array is : 2 4 6
    Next bigger element of 2 in the array is : 4       
    Next bigger element of 4 in the array is : 6       
    Next bigger element of 6 in the array is : -1   
    
    Test case 2 :The given array is : 54 12 13
    Next bigger element of 54 in the array is : -1
    Next bigger element of 12 in the array is : 13
    Next bigger element of 13 in the array is : -1

---
+++ ArrayNextGreater.c
@@ -0,0 +1,47 @@
+//To print the next greater no. in an array
+
+#include<stdio.h>
+
+void nextGreater(int arr[],int n)
+{
+    int nextBigger[n];
+    int k=0,t=0;
+    nextBigger[k]=-1; //case if no bigger element present
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+        if(arr[i]<arr[j])
+        {
+            nextBigger[k]=arr[j];
+            break;
+        }
+        }
+        printf("Next bigger element of %d in the array is : %d\n",arr[i],nextBigger[k]);
+        nextBigger[t]=nextBigger[k];
+        k++;
+        t++;
+        nextBigger[k]=-1;
+
+    }
+    printf("Next Bigger Elements Array : \n"); 
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ",nextBigger[i]);
+    }
+}
+int main()
+{
+    int n;
+    printf("Enter the size of the array : ");
+    scanf("%d",&n);
+	int arr[n];
+    printf("The given array is :\n");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    
+	nextGreater(arr,n);
+	return 0;
+}
